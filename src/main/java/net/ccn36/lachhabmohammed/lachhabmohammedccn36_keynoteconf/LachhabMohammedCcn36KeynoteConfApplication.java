1008fc1f2b0e633f7a58bc5323020991849dfe2a
Lachhab1 <Mohamed172lachhab@gmail.com>
2025-10-24 18:12:01 +0100

    project setup

---
+++ src/main/java/net/ccn36/lachhabmohammed/lachhabmohammedccn36_keynoteconf/LachhabMohammedCcn36KeynoteConfApplication.java
@@ -0,0 +1,13 @@
+package net.ccn36.lachhabmohammed.lachhabmohammedccn36_keynoteconf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LachhabMohammedCcn36KeynoteConfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LachhabMohammedCcn36KeynoteConfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/ccn36/lachhabmohammed/lachhabmohammedccn36_keynoteconf/LachhabMohammedCcn36KeynoteConfApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ccn36.lachhabmohammed.lachhabmohammedccn36_keynoteconf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LachhabMohammedCcn36KeynoteConfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
